7aec28d7d150e8f2be4c99b227dbdc26392fb767
shukvino <shukvino@amazon.com>
2020-08-08 15:11:10 -0400

    Refactored Lambda to be triggered from SQS instead of polling

--- functions/source/integration/main.js
+++ functions/source/integration/main.js
@@ -1,191 +1,207 @@
-var AWS = require('aws-sdk'),
-	util = require('util'),
-	Promise = require('bluebird'),
-    conf = Promise.promisifyAll(require('./configHelper')),
-    s3 = Promise.promisifyAll(require('node-s3-encryption-client')),
-    awsS3 = new AWS.S3(),
-    sftpHelper = require('./sftpHelper'),
-    json2csv = require('./jsontocsv'),
-    sqs = new AWS.SQS();
-var objIsCSV  = true;
+const AWS = require('aws-sdk');
+const util = require('util');
+const Promise = require('bluebird');
+const conf = Promise.promisifyAll(require('./configHelper'));
+const s3 = new AWS.S3();
+const sftpHelper = require('./sftpHelper');
+const json2csv = require('./jsontocsv');
+const sqs = new AWS.SQS();
 
-exports.handle = function(event, context) {
-    console.log(event);
-    console.log(context);
-	return exports.pollSqs(context);
+var objIsCSV = true;
+
+exports.handle = async (event, context, callback) => {
+    console.log(JSON.stringify(event));
+    console.log(JSON.stringify(context));
+
+    var records = event.Records;
+    for (let index = 0; index < records.length; index++) {
+
+        // Read options from the event parameter.
+        //console.log("Reading options from event:\n", util.inspect(event, {depth: 5}));
+
+
+        var eventJson = JSON.parse(records[index].body);
+        console.log(eventJson.Records);
+
+        var bodyRecords = eventJson.Records;
+
+
+        for (let indexJ = 0; indexJ < bodyRecords.length; indexJ++) {
+
+            var srcBucket = bodyRecords[indexJ].s3.bucket.name;
+            // Object key may have spaces or unicode non-ASCII characters.
+            var srcKey = decodeURIComponent(bodyRecords[indexJ].s3.object.key.replace(/\+/g, " "));
+
+            try {
+                const params = {
+                    Bucket: srcBucket,
+                    Key: srcKey
+                };
+                var s3Obj = await s3.getObject(params).promise();
+                var config = await conf.getConfigAsync(context);
+                var sftpConfig = await exports.getSftpConfig(config);
+
+            } catch (error) {
+                console.log(error);
+    //            return;
+            }
+
+            console.info("srcKey: " + srcKey);
+            var fullS3Path = srcBucket + '/' + srcKey;
+            console.info("Object path: " + fullS3Path + " | config s3 Loc: " + config["s3Location"]);
+            var newObjectS3Path = exports.getFilePathArray(fullS3Path);
+
+            let objectData = s3Obj.Body.toString('utf-8');
+
+            console.log(objectData);
+
+            // Do not process Calabrio_ServiceHistorical or Calabrio_AgentProductivity CSV reports and move on//
+            if (srcKey.match(/Calabrio/i)) {
+                console.info("Key " + srcKey + " is a Calabrio Report. Stop processing and retrieve next file");
+                //          return;
+            }
+
+            // Do not process WAV files and move on
+            if (srcKey.match(/\.wav$/i)) {
+                console.info("Key " + srcKey + " is a wav file. Stop processing and retrieve next file");
+    //            return;
+            }
+
+            if (!objectData.Metadata || objectData.Metadata.synched !== "true") {
+                //console.info("New Object path: " + newObjectS3Path);
+                var configKeys = Object.keys(config)//.filter(function(key) {
+                if (configKeys.length === 0) console.warn("No configured SFTP destination for " + fullS3Path);
+                var s3Location = config["s3Location"];
+
+                try {
+                    if (s3Location) {
+                        console.info("Configkeys: " + Object.keys(config));
+                        var configS3Path = exports.getFilePathArray(s3Location);
+                    }
+                } catch (err) {
+                    console.info("Error caught in exports.getFilePathArray(s3Location): " + err);
+                }
+
+                var bodydata = objectData;
+
+                if (srcKey.match(/\.csv$/) === null) {
+                    var msg = "Key " + srcKey + " is not a csv file, attempting CTR conversion";
+                    objIsCSV = false;
+                    console.log(msg);
+                } else {
+                    objIsCSV = true;
+                }
+
+                if (!objIsCSV) {
+                    try {
+                        bodydata = json2csv.jsonconvert(objectData);
+                        console.info("Returned from json2csv.jsonconvert successfully");
+                    } catch (err) {
+                        console.info("Error in json2csv.jsonconvert: " + err);
+                        console.info("json2csv.jsonconvert(objectData): " + objectData);
+    //                    return err;
+                    }
+                }
+
+            }
+            console.info("s3Location: " + s3Location);
+            try {
+                var configS3Path = exports.getFilePathArray(config["s3Location"]);
+            } catch (err) {
+                console.info("Error in getFilePathArray: " + err);
+            }
+            try {
+                var sftpDirPath = exports.getFilePathArray(config["sftpLocation"]);
+            } catch (err) {
+                console.info("Error in getFilePathArray: " + err);
+            }
+            console.info("configS3Path: " + configS3Path + " | sftpDirPath:" + sftpDirPath);
+
+            let sftpObj = sftpHelper.withSftpClient(sftpConfig, function (sftp) {
+                console.info("Returned from sftpHelper.withSftpClient");
+                var sftpFileName = sftpDirPath.concat(newObjectS3Path[newObjectS3Path.length - 1].replace(/:/g, '_')).join('/');
+                console.info("sftpFileName");
+                if (!objIsCSV) {
+                    sftpFileName += ".csv";
+                }
+                console.log("Calling sftpHelper.writeFile");
+                return sftpHelper.writeFile(
+                    sftp,
+                    sftpFileName,
+                    bodydata
+                )
+                    .then(function () {
+                        console.info("...done");
+                        console.info("[" + sftpFileName + "]: Moved 1 files from S3 to SFTP");
+                        return sftpFileName;
+                    })
+                    .catch(function (err) {
+                        console.log(err);
+                    });
+            });
+
+            try {
+                await sftpObj;
+            } catch (error) {
+                console.log(error);
+    //            return;
+            }
+        }
+    }
 };
 
-exports.pollSqs = function(context) {
-  return sqs.getQueueUrl({
-    QueueName: context.functionName
-  }).promise()
-  .then(function(queueData) {
-    return Promise.mapSeries(
-      Array.apply(null, {length: 10}).map(Number.call, Number), 
-      function(i) {
-        return sqs.receiveMessage({
-          QueueUrl: queueData.QueueUrl,
-          MaxNumberOfMessages: 10
-        }).promise()
-        .then(function(messages) {
-	          return Promise.mapSeries(
-	            messages.Messages || [],
-	            function(message) {
-	              return internalNewS3Object(JSON.parse(message.Body), context)
-	              .then(function(results) {
-	                return sqs.deleteMessage({
-	                  QueueUrl: queueData.QueueUrl,
-	                  ReceiptHandle: message.ReceiptHandle
-	                }).promise()
-	                .then(function(data) {
-	                  return results;
-	                });
-	              });
-	            });
-        });
-      }
-    );
-  });
+exports.getFilePathArray = function (filePath) {
+    return (filePath || '').split('/').filter(function (s) {
+        return s ? true : false
+    });
 };
 
-function internalNewS3Object(event, context) {
-	return Promise.try(function () {
-		if (!event.Records) {
-			console.log("Not a valid record entry.");
-		} else {
-			console.info("Retrieving sftp variables");
-			return conf.getConfigAsync(context)
-				.then(function (config) {
-					    console.log("Config: \n", config);
-						return Promise.map(
-							event.Records,
-							function (record) {
-								//console.log("Reading options from event:\n", util.inspect(event, {depth: 5}));
-								console.log("Record: \n", record);
-								var srcBucket = event.Records[0].s3.bucket.name;
-								var srcKey = event.Records[0].s3.object.key;
-								var fullS3Path = record.s3.bucket.name + '/' + decodeURIComponent(record.s3.object.key);
-								console.info("Object path: " + fullS3Path + " | config s3 Loc: " + config["s3Location"]);
-								var newObjectS3Path = exports.getFilePathArray(fullS3Path);
+exports.getSftpConfig = function (config) {
+    return Promise.try(function () {
+        if (!config["host"]) throw new Error("SFTP config not found");
+        console.info("Host found: " + config["host"]);
+        var sftpconfig = {
+            "host": config["host"],
+            "port": config["port"],
+            "username": config["username"],
+            "password": config["password"],
+        };
+        if (config["s3PrivateKey"]) {
+            var bucketDelimiterLocation = config.sftpConfig.s3PrivateKey.indexOf("/");
 
-								var getParams = {
-									Bucket: record.s3.bucket.name,
-									Key: decodeURIComponent(record.s3.object.key)
-								};
+            var getParams = {
+                Bucket: config.sftpConfig.s3PrivateKey.substr(0, bucketDelimiterLocation),
+                Key: config.sftpConfig.s3PrivateKey.substr(bucketDelimiterLocation + 1)
+            };
 
-								var s3obj = s3.getObject(getParams, function (err, data) {
-									// Handle any error and exit
-									if (err)
-										return err;
+            var s3obj = s3.getObject(getParams, function (err, data) {
+                // Handle any error and exit
+                if (err) {
+                    console.info("getSftpConfig: Error caught in s3.getObject: " + err);
+                    return err;
+                }
 
-									// No error happened
-									// Convert Body from a Buffer to a String
-
-									let objectData = data.Body.toString('utf-8');
-									if (!objectData.Metadata || objectData.Metadata.synched != "true") {
-										//console.info("New Object path: " + newObjectS3Path);
-										var configKeys = Object.keys(config)//.filter(function(key) {
-										if (configKeys.length === 0) console.warn("No configured SFTP destination for " + fullS3Path);
-										var s3Location = config["s3Location"];
-										if (s3Location) {
-											//console.info("Configkeys: " + Object.keys(config));
-											var configS3Path = exports.getFilePathArray(s3Location);
-										}
-
-										var bodydata = objectData;
-
-										if (srcKey.match(/\.csv$/) === null) {
-											var msg = "Key " + srcKey + " is not a csv file, attempting CTR conversion";
-											objIsCSV = false;
-											console.log(msg);
-										} else {
-											objIsCSV = true;
-										}
-
-										if (!objIsCSV) {
-											bodydata = json2csv.jsonconvert(objectData);
-										}
-
-									}
-									var configS3Path = exports.getFilePathArray(config["s3Location"]);
-									var sftpDirPath = exports.getFilePathArray(config["sftpLocation"]);
-									//console.info("configS3Path: " + configS3Path + " | sftpDirPath:" + sftpDirPath);
-									return exports.getSftpConfig(config)
-										.then(function (sftpConfig) {
-											return sftpHelper.withSftpClient(sftpConfig, function (sftp) {
-												var sftpFileName = sftpDirPath.concat(newObjectS3Path[newObjectS3Path.length - 1].replace(/:/g, '_')).join('/');
-												if (!objIsCSV) {
-													sftpFileName += ".csv";
-												}
-												return sftpHelper.writeFile(
-													sftp,
-													sftpFileName,
-													bodydata
-												)
-													.then(function () {
-														console.info("...done");
-														console.info("[" + sftpFileName + "]: Moved 1 files from S3 to SFTP");
-														return sftpFileName;
-													});
-											});
-										})
-										.catch(function(err) {
-											console.log(err);
-										});
-								});
-							});
-					}
-				);
-		}
-	});
-	//}
-}
-
-exports.getFilePathArray = function(filePath) {
-  return (filePath || '').split('/').filter(function(s) { return s ? true : false });
-};
-
-exports.getSftpConfig = function(config) {
-  return Promise.try(function() {
-    if (!config["host"]) throw new Error("SFTP config not found");
-    console.info("Host found: " + config["host"]);
-    var sftpconfig = {
-    		"host" : config["host"],
-    		"port" : config["port"],
-    		"username" : config["username"],
-    		"password" : config["password"],
-    };
-    if (config["s3PrivateKey"]) {
-      var bucketDelimiterLocation = config.sftpConfig.s3PrivateKey.indexOf("/");
-
-	  var getParams = {
-			Bucket: config.sftpConfig.s3PrivateKey.substr(0, bucketDelimiterLocation),
-				Key: config.sftpConfig.s3PrivateKey.substr(bucketDelimiterLocation + 1)
-	  };
-
-	  var s3obj = s3.getObject(getParams, function (err, data) {
-			// Handle any error and exit
-			if (err)
-				return err;
-
-		  let objectData = data.Body.toString('utf-8');
-		  sftpconfig.privateKey = objectData;
-		  //delete config.s3PrivateKey;
-		  return sftpconfig;
-	  });
-    } else return sftpconfig;
-  });
+                let objectData = data.Body.toString('utf-8');
+                sftpconfig.privateKey = objectData;
+                //delete config.s3PrivateKey;
+                return sftpconfig;
+            });
+        } else {
+            console.info("Returning sftpconfig");
+            return sftpconfig;
+        }
+    });
 };
 
 function flatten(arr) {
-  return arr.reduce(function(a, b) {
-    if (Array.isArray(b)) {
-      return a.concat(flatten(b));
-    } else if (b) {
-      a.push(b);
-      return a;
-    } else {
-      return a;
-    }
-  }, []);
+    return arr.reduce(function (a, b) {
+        if (Array.isArray(b)) {
+            return a.concat(flatten(b));
+        } else if (b) {
+            a.push(b);
+            return a;
+        } else {
+            return a;
+        }
+    }, []);
 }
